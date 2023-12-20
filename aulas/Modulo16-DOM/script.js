010bb4343614c8d8fb707678f0ec1b048aef45ea
Roger Herany <caminharoger@gmail.com>
2023-12-20 19:48:52 +0100

    att dom

--- aulas/Modulo16-DOM/script.js
+++ aulas/Modulo16-DOM/script.js
@@ -1,1 +1,5 @@
-alert('Olá');
+document.querySelector("ul").lastElementChild.innerHTML="Roger";
+
+document.querySelector("li a").style.color="red";
+
+document.querySelector("button").style.backgroundColor="yellow";
