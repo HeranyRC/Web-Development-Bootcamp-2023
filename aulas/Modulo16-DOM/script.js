ec2726020311910fecffaeb9eb22ba630a9207df
Roger Herany <caminharoger@gmail.com>
2023-12-22 13:02:55 +0100

    Modulo concluido, novo desafio

--- aulas/Modulo16-DOM/script.js
+++ aulas/Modulo16-DOM/script.js
@@ -1,5 +1,11 @@
-document.querySelector("ul").lastElementChild.innerHTML="Roger";
+document.querySelector("ul").lastElementChild.innerHTML="<em>Roger</em>";
 
 document.querySelector("li a").style.color="red";
 
 document.querySelector("button").style.backgroundColor="yellow";
+
+document.querySelector("button").classList.add("invisivel"); /*inves do add pode ser usado o toggle que funciona como um interruptor que liga se estiver desligado e vice versa*/
+
+document.querySelector("h1").classList.toggle("gigante");
+
+document.querySelector("a").setAttribute("href","https://www.bing.com");
