686f1396e814433b926f2305392cb601d0c79934
Roger Herany <caminharoger@gmail.com>
2023-12-23 23:49:46 +0100

    Melhoria do desafio

--- desafios/Dicee Challenge - Starting Files/script.js
+++ desafios/Dicee Challenge - Starting Files/script.js
@@ -1,47 +1,12 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomSource1="images/dice"+randomNumber1+".png";
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomSource1);
+
 var randomNumber2=Math.floor(Math.random()*6)+1;
-
-switch (randomNumber1) {
-    case 1:
-        document.querySelector(".img1").setAttribute("src","images/dice1.png");
-    break;
-    case 2:
-        document.querySelector(".img1").setAttribute("src","images/dice2.png");
-    break;
-    case 3:
-        document.querySelector(".img1").setAttribute("src","images/dice3.png");
-    break;
-    case 4:
-        document.querySelector(".img1").setAttribute("src","images/dice4.png");
-    break;
-    case 5:
-        document.querySelector(".img1").setAttribute("src","images/dice5.png");
-    break;
-    case 6:
-        document.querySelector(".img1").setAttribute("src","images/dice6.png");
-    break;
-}
-
-switch (randomNumber2) {
-    case 1:
-        document.querySelector(".img2").setAttribute("src","images/dice1.png");
-    break;
-    case 2:
-        document.querySelector(".img2").setAttribute("src","images/dice2.png");
-    break;
-    case 3:
-        document.querySelector(".img2").setAttribute("src","images/dice3.png");
-    break;
-    case 4:
-        document.querySelector(".img2").setAttribute("src","images/dice4.png");
-    break;
-    case 5:
-        document.querySelector(".img2").setAttribute("src","images/dice5.png");
-    break;
-    case 6:
-        document.querySelector(".img2").setAttribute("src","images/dice6.png");
-    break;
-}
+var randomSource2="images/dice"+randomNumber2+".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomSource2);
 
 if (randomNumber1===randomNumber2)
     document.querySelector("h1").innerHTML="Draw";
