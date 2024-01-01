4e812b0c4401d327fe13e46ef6c12fa09df0d6dd
Roger Herany <caminharoger@gmail.com>
2024-01-02 00:28:42 +0100

    Att

--- aulas/Modulo18-AdvancedDOM/Drum kit/script.js
+++ aulas/Modulo18-AdvancedDOM/Drum kit/script.js
@@ -3,6 +3,7 @@ var numeroDeBotoes= document.querySelectorAll(".drum").length;
 
 for(i=0;i<numeroDeBotoes;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",clicado);
+    
 }
 /*
 document.querySelector(".w").addEventListener("click",clicado);
@@ -14,5 +15,47 @@ document.querySelector(".k").addEventListener("click",clicado);
 document.querySelector(".l").addEventListener("click",clicado);
 */
 function clicado(){
-    alert("Fui Clicado!");
+    var botao=this.innerHTML;
+
+    switch (botao) {
+        case "w":
+            var tom1= new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2= new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3= new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4= new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            var crash= new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "k":
+            var kickBass= new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+        break;
+
+        case "l":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+
+        default:
+            alert(botao);
+    }
 }
+
+
