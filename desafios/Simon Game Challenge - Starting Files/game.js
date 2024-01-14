78c5339bec39fc806824ccf3ed45672826ea10d2
Roger Herany <caminharoger@gmail.com>
2024-01-14 20:11:21 +0100

    Desafio Simon Game

---
+++ desafios/Simon Game Challenge - Starting Files/game.js
@@ -0,0 +1,9 @@
+
+var buttonColours=["green","red","yellow","blue"];
+var gamePattern=[];
+
+function nextSequence() {
+    var random= Math.floor(Math.random()*4);
+    var randomChosenColour;
+}
+
